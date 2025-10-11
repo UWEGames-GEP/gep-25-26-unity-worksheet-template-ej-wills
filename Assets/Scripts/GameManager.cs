3f1728e21e5a9394bb1294e3105bada43ad514db
Ewanwills <ewan4.williams@live.uwe.ac.uk>
2025-10-11 13:10:36 +0100

    Basic task done
    pause works

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,8 @@ public class GameManager : MonoBehaviour
 
     enum GameStates {GamePlay, Pause};
     [SerializeField] GameStates  state ;
+    private bool hasChanged = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,18 +20,41 @@ public class GameManager : MonoBehaviour
     {
        if(state == GameStates.GamePlay)
        {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if(Input.GetKeyDown(KeyCode.Return))
         {
             state = GameStates.Pause;
+            hasChanged = true;
         }
        } 
 
        else if (state == GameStates.Pause)
        {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if(Input.GetKeyDown(KeyCode.Return))
         {
             state = GameStates.GamePlay;
+            hasChanged = true;
         }
        }
     }
+
+    private void LateUpdate()
+    {
+        if(hasChanged)
+        {
+            hasChanged = false;
+        
+
+        if (state == GameStates.GamePlay)
+        {
+            Time.timeScale = 1.0f;
+        }
+        else if (state == GameStates.Pause)
+        {
+            Time.timeScale = 0.0f;
+
+        }
+        }
+
+    }
+
 }
