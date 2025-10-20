def412e617a3ba699470857e0ce988db6cb7a548
ej-wills <ewan4.williams@live.uwe.ac.uk>
2025-10-20 15:18:50 +0100

    .

--- Assets/Scripts/ItemObject.cs
+++ Assets/Scripts/ItemObject.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class ItemObject : MonoBehaviour
 {
 
-    public int weight;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
